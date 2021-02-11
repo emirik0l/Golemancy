7252c2fadad77616a1df6fefbdb1c72872b5ea89
Emirikol <77687278+emirik0l@users.noreply.github.com>
2021-02-11 23:17:05 +0000

    Update Breeding.java

--- src/main/java/net/emirikol/amm/genetics/Breeding.java
+++ src/main/java/net/emirikol/amm/genetics/Breeding.java
@@ -10,18 +10,20 @@ import net.minecraft.entity.mob.*;
 
 import java.util.*;
 
+//A collection of static methods related to breeding and mutating soulstones.
 public class Breeding {
-	//Create a new genome for a soulstone by breeding two parents.
+	
+	//Create a new "child" soulstone by breeding two parents.
 	//Uses mendelian inheritance to randomly assign genes from the parents to the child.
 	public static ItemStack breed(ItemStack parent1, ItemStack parent2) {
 		Random rand = new Random();
 		//Create a placeholder ItemStack to hold genes until we figure out what species the child should be.
 		Soulstone soulstone = (Soulstone) parent1.getItem();
 		ItemStack stack = new ItemStack(soulstone);
+		Genome newGenome = soulstone.getGenome(stack);
 		//Extract genomes from parents and create a new genome for this stack.
 		Genome genome1 = ((Soulstone) parent1.getItem()).getGenome(parent1);
 		Genome genome2 = ((Soulstone) parent2.getItem()).getGenome(parent2);
-		Genome newGenome = soulstone.getGenome(stack);
 		Genome.Genepool[] options = {genome1.dominant, genome1.recessive, genome2.dominant, genome2.recessive};
 		//Generate dominant genepool.
 		for (String key : newGenome.dominant.alleles.keySet()) {
