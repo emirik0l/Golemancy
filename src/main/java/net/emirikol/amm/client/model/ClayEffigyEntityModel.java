3b6503b9e7147efe74e2e220350ad654f666043e
Emirikol <77687278+emirik0l@users.noreply.github.com>
2021-02-26 00:07:15 +0000

    improving clay effigy model further
    also added a temporary "wander far" goal so i can test walking animations

--- src/main/java/net/emirikol/amm/client/model/ClayEffigyEntityModel.java
+++ src/main/java/net/emirikol/amm/client/model/ClayEffigyEntityModel.java
@@ -8,32 +8,60 @@ import net.minecraft.client.render.entity.model.*;
 import net.minecraft.client.util.math.*;
 
 public class ClayEffigyEntityModel extends EntityModel<ClayEffigyEntity> {
-	private final ModelPart base;
+	private final ModelPart Body;
+	private final ModelPart Head;
+	private final ModelPart LeftLeg;
+	private final ModelPart RightLeg;
+	private final ModelPart LeftArm;
+	private final ModelPart RightArm;
 	
 	public ClayEffigyEntityModel() {
-		super(RenderLayer::getEntityCutout);
-		//The default texture size is 64 wide and 32 tall.
-		this.textureHeight = 64;
-		this.textureWidth = 64;
-		base = new ModelPart(this, 0, 0);
-		base.setTextureOffset(0, 0).addCuboid(-4.0F, -6.0F, -3.0F, 7.0F, 7.0F, 7.0F, 0.0F, true); //body
-		base.setTextureOffset(44, 54).addCuboid(-3.0F, -11.0F, -2.0F, 5.0F, 5.0F, 5.0F, 0.0F, true); //head
-		base.setTextureOffset(0, 0).addCuboid(-3.0F, -6.0F, -5.0F, 5.0F, 9.0F, 2.0F, 0.0F, false); //arm
-		base.setTextureOffset(0, 0).addCuboid(-3.0F, -6.0F, 4.0F, 5.0F, 9.0F, 2.0F, 0.0F, false); //arm
-		base.setTextureOffset(0, 0).addCuboid(-4.0F, 1.0F, 0.0F, 7.0F, 5.0F, 4.0F, 0.0F, false); //leg
-		base.setTextureOffset(0, 0).addCuboid(-4.0F, 1.0F, -3.0F, 7.0F, 5.0F, 3.0F, 0.0F, false); //leg
+		textureWidth = 32;
+		textureHeight = 32;
+		Body = new ModelPart(this);
+		Body.setPivot(0.0F, 10.5F, 0.0F);
+		Body.setTextureOffset(0, 0).addCuboid(-4.0F, -4.5F, -3.0F, 8.0F, 9.0F, 6.0F, 0.0F, false);
+
+		Head = new ModelPart(this);
+		Head.setPivot(0.0F, 3.5F, 0.0F);
+		Head.setTextureOffset(8, 21).addCuboid(-3.0F, -2.5F, -3.0F, 6.0F, 5.0F, 6.0F, 0.0F, false);
+
+		LeftLeg = new ModelPart(this);
+		LeftLeg.setPivot(2.0F, 15.5F, 0.0F);
+		LeftLeg.setTextureOffset(0, 0).addCuboid(-2.0F, -0.5F, -3.0F, 4.0F, 9.0F, 6.0F, 0.0F, false);
+
+		RightLeg = new ModelPart(this);
+		RightLeg.setPivot(-2.0F, 15.5F, 0.0F);
+		RightLeg.setTextureOffset(0, 0).addCuboid(-2.0F, -0.5F, -3.0F, 4.0F, 9.0F, 6.0F, 0.0F, false);
+
+		LeftArm = new ModelPart(this);
+		LeftArm.setPivot(4.5F, 6.0F, 0.0F);
+		LeftArm.setTextureOffset(0, 0).addCuboid(-0.5F, 0.0F, -3.0F, 3.0F, 10.0F, 6.0F, 0.0F, false);
+
+		RightArm = new ModelPart(this);
+		RightArm.setPivot(-4.5F, 6.0F, 0.0F);
+		RightArm.setTextureOffset(0, 0).addCuboid(-2.5F, 0.0F, -3.0F, 3.0F, 10.0F, 6.0F, 0.0F, false);
 	}
 	
 	@Override
-	public void setAngles(ClayEffigyEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-		//TODO
+	public void setAngles(ClayEffigyEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+			//previously the render function, render code was moved to a method below
 	}
 	
 	@Override
-	public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
-		//Translate the model down slightly, because standard vanilla models appear higher than the entity hitbox.
-		matrices.translate(0, 1.125, 0);
-		//Render the model.
-		base.render(matrices, vertices, light, overlay);
+	public void render(MatrixStack matrixStack, VertexConsumer	buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+			
+			Body.render(matrixStack, buffer, packedLight, packedOverlay);
+			Head.render(matrixStack, buffer, packedLight, packedOverlay);
+			LeftLeg.render(matrixStack, buffer, packedLight, packedOverlay);
+			RightLeg.render(matrixStack, buffer, packedLight, packedOverlay);
+			LeftArm.render(matrixStack, buffer, packedLight, packedOverlay);
+			RightArm.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+	
+	public void setRotationAngle(ModelPart bone, float x, float y, float z) {
+			bone.pitch = x;
+			bone.yaw = y;
+			bone.roll = z;
 	}
 }
--- src/main/java/net/emirikol/amm/entity/ClayEffigyEntity.java
+++ src/main/java/net/emirikol/amm/entity/ClayEffigyEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.*;
 import net.minecraft.entity.mob.*;
 import net.minecraft.entity.passive.*;
 import net.minecraft.entity.attribute.*;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.world.*;
 import net.minecraft.server.world.*;
 
@@ -19,6 +20,12 @@ public class ClayEffigyEntity extends TameableEntity {
    
 	public static DefaultAttributeContainer.Builder createClayEffigyAttributes() {
 		return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25).add(EntityAttributes.GENERIC_MAX_HEALTH, 4.0D).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0D);
+	}
+	
+	@Override
+	protected void initGoals() {
+		//Temporary to test effigy model.
+		this.goalSelector.add(8, new WanderAroundFarGoal(this, 0.75D));
 	}
 	
 	public ClayEffigyEntity createChild(ServerWorld serverWorld, PassiveEntity passiveEntity) {
