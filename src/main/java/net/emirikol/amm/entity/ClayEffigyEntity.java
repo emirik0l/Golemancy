e583fcdecf069189c20718def31fa225523ebe94
Emirikol <77687278+emirik0l@users.noreply.github.com>
2021-02-27 19:12:18 +0000

    add a simple "eat item" goal to the hungry golem
    no animations yet

--- src/main/java/net/emirikol/amm/entity/ClayEffigyEntity.java
+++ src/main/java/net/emirikol/amm/entity/ClayEffigyEntity.java
@@ -126,6 +126,7 @@ public class ClayEffigyEntity extends TameableEntity {
 	}
 	
 	@Override protected void initGoals() {
+		this.goalSelector.add(5, new GolemEatHeldItemGoal(this));
 		this.goalSelector.add(6, new GolemTakeItemGoal(this, 10.0F));
 		this.goalSelector.add(6, new GolemFollowOwnerGoal(this, 1.0D, 6.0F, 2.0F, 750.0F, false));
 		this.goalSelector.add(8, new GolemWanderAroundFarGoal(this, 1.0D));
---
+++ src/main/java/net/emirikol/amm/entity/goal/GolemEatHeldItemGoal.java
@@ -0,0 +1,34 @@
+package net.emirikol.amm.entity.goal;
+
+import net.emirikol.amm.entity.*;
+
+import net.minecraft.item.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.util.math.*;
+
+import java.util.*;
+
+public class GolemEatHeldItemGoal extends Goal {
+	private static final List<String> VALID_TYPES = Arrays.asList(new String[]{"Hungry"});
+	
+	private final ClayEffigyEntity entity;
+	
+	public GolemEatHeldItemGoal(ClayEffigyEntity entity) {
+		this.entity = entity;
+	}
+	
+	public boolean canStart() {
+		//Check if the golem is the correct type for this behaviour.
+		String golemType = entity.getGolemType();
+		if (!VALID_TYPES.contains(golemType)) {
+			return false;
+		}
+		return !entity.getEquippedStack(EquipmentSlot.MAINHAND).isEmpty();
+	}
+	
+	public void start() {
+		//TODO - add some kind of animation
+		entity.equipStack(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
+	}
+}
