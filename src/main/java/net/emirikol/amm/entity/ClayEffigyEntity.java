304c10f1d0e4b9c50233ed78209e38fd97fa9799
Emirikol <77687278+emirik0l@users.noreply.github.com>
2021-03-01 21:15:39 +0000

    decrease priority of golem following

--- src/main/java/net/emirikol/amm/entity/ClayEffigyEntity.java
+++ src/main/java/net/emirikol/amm/entity/ClayEffigyEntity.java
@@ -42,7 +42,7 @@ public class ClayEffigyEntity extends TameableEntity {
 	protected void initGoals() {
 		this.goalSelector.add(5, new GolemEatHeldItemGoal(this, new String[]{"Hungry"}));
 		this.goalSelector.add(6, new GolemMoveToItemGoal(this, 10.0F, new String[]{"Hungry", "Covetous"}));
-		this.goalSelector.add(6, new GolemFollowOwnerGoal(this, 1.0D, 6.0F, 2.0F, 750.0F, false, new String[]{"Curious"}));
+		this.goalSelector.add(7, new GolemFollowOwnerGoal(this, 1.0D, 6.0F, 2.0F, 750.0F, false, new String[]{"Curious"}));
 		this.goalSelector.add(8, new GolemWanderAroundFarGoal(this, 1.0D, new String[]{"Restless"}));
 		this.goalSelector.add(10, new GolemLookAtEntityGoal(this, PlayerEntity.class, 8.0F, new String[]{"Restless", "Curious", "Hungry", "Covetous"}));
 	}
