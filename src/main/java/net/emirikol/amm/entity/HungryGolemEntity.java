4ad529fb9d7bb27c3675223d2c7bc970a1c4e675
Emirikol <77687278+emirik0l@users.noreply.github.com>
2021-03-07 19:50:52 +0000

    allow hungry golems to eat items from a linked container

--- src/main/java/net/emirikol/amm/entity/HungryGolemEntity.java
+++ src/main/java/net/emirikol/amm/entity/HungryGolemEntity.java
@@ -21,6 +21,7 @@ public class HungryGolemEntity extends AbstractGolemEntity {
 	protected void initGoals() {
 		super.initGoals();
 		this.goalSelector.add(5, new GolemEatHeldItemGoal(this));
+		this.goalSelector.add(5, new GolemExtractItemGoal(this));
 		this.goalSelector.add(6, new GolemMoveToItemGoal(this, 10.0F));
 	}
 	
