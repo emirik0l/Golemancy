f0db2d37efbf407a781393f3637d280b7dd105e7
Emirikol <77687278+emirik0l@users.noreply.github.com>
2021-03-15 00:29:57 +0000

    add goals to intrepid golem

--- src/main/java/net/emirikol/golemancy/entity/IntrepidGolemEntity.java
+++ src/main/java/net/emirikol/golemancy/entity/IntrepidGolemEntity.java
@@ -7,13 +7,17 @@ import net.minecraft.entity.*;
 import net.minecraft.entity.mob.*;
 import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.*;
+import net.minecraft.entity.projectile.thrown.*;
+import net.minecraft.entity.ai.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.world.*;
+import net.minecraft.sound.*;
+import net.minecraft.util.math.*;
 import net.minecraft.server.world.*;
 
 import java.util.*;
 
-public class IntrepidGolemEntity extends AbstractGolemEntity {
+public class IntrepidGolemEntity extends AbstractGolemEntity implements RangedAttackMob {
 	public IntrepidGolemEntity(EntityType<? extends IntrepidGolemEntity> entityType, World world) {
 		super(entityType, world);
 	}
@@ -21,6 +25,12 @@ public class IntrepidGolemEntity extends AbstractGolemEntity {
 	@Override 
 	protected void initGoals() {
 		super.initGoals();
+		this.goalSelector.add(1, new ProjectileAttackGoal(this, 1.25D, 20, 10.0F));
+		this.targetSelector.add(1, new TrackOwnerAttackerGoal(this));
+		this.targetSelector.add(2, new AttackWithOwnerGoal(this));
+		this.targetSelector.add(3, new FollowTargetGoal(this, MobEntity.class, 5, false, false, (livingEntity) -> {
+			return livingEntity instanceof Monster && !(livingEntity instanceof CreeperEntity);
+		}));
 	}
 	
 	@Override
@@ -33,5 +43,18 @@ public class IntrepidGolemEntity extends AbstractGolemEntity {
 			golemEntity.setTamed(true);
 		}
 		return golemEntity;
+	}
+	
+	@Override
+	public void attack(LivingEntity target, float pullProgress) {
+		SnowballEntity snowballEntity = new SnowballEntity(this.world, this);
+		double d = target.getEyeY() - 1.100000023841858D;
+		double e = target.getX() - this.getX();
+		double f = d - snowballEntity.getY();
+		double g = target.getZ() - this.getZ();
+		float h = MathHelper.sqrt(e * e + g * g) * 0.2F;
+		snowballEntity.setVelocity(e, f + (double)h, g, 1.6F, 12.0F);
+		this.playSound(SoundEvents.ENTITY_SNOW_GOLEM_SHOOT, 1.0F, 0.4F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
+		this.world.spawnEntity(snowballEntity);
 	}
 }
