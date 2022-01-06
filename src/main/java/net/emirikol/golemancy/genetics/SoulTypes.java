b023ea4c86416e048f3f71a3aa46e146e502203a
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-06 14:58:21 +0000

    fix a crash caused by GENERIC SoulType having a null EntityType and not being included in SOUL_TYPES

--- src/main/java/net/emirikol/golemancy/genetics/SoulTypes.java
+++ src/main/java/net/emirikol/golemancy/genetics/SoulTypes.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class SoulTypes {
-    public static final SoulType GENERIC = new SoulType("Generic", null);
+    public static final SoulType GENERIC = new SoulType("Generic", Golemancy.RESTLESS_GOLEM_ENTITY);
 
     public static final SoulType COVETOUS = new SoulType("Covetous", Golemancy.COVETOUS_GOLEM_ENTITY);
     public static final SoulType CURIOUS = new SoulType("Curious", Golemancy.CURIOUS_GOLEM_ENTITY);
@@ -24,7 +24,7 @@ public class SoulTypes {
     public static final SoulType VALIANT = new SoulType("Valiant", Golemancy.VALIANT_GOLEM_ENTITY);
     public static final SoulType WEEPING = new SoulType("Weeping", Golemancy.WEEPING_GOLEM_ENTITY);
 
-    public static final List<SoulType> SOUL_TYPES = Arrays.asList(COVETOUS, CURIOUS, ENTROPIC, HUNGRY, INTREPID, PARCHED, RESTLESS, TACTILE, VALIANT, WEEPING);
+    public static final List<SoulType> SOUL_TYPES = Arrays.asList(GENERIC, COVETOUS, CURIOUS, ENTROPIC, HUNGRY, INTREPID, PARCHED, RESTLESS, TACTILE, VALIANT, WEEPING);
 
     public static SoulType get(String typeString) {
         //Find a SoulType from the type string, i.e. "Curious".
