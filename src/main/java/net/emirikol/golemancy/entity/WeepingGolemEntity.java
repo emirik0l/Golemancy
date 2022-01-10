e4722cb88f6d93941fa8b9b8a40b2b31be75b06c
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-10 09:28:39 +0000

    Update WeepingGolemEntity.java

--- src/main/java/net/emirikol/golemancy/entity/WeepingGolemEntity.java
+++ src/main/java/net/emirikol/golemancy/entity/WeepingGolemEntity.java
@@ -19,7 +19,7 @@ public class WeepingGolemEntity extends AbstractGolemEntity {
 	protected void initGoals() {
 		super.initGoals();
 		this.goalSelector.add(5, new GolemHealGoal(this, 5.0F));
-		this.goalSelector.add(6, new GolemMoveToHealGoal(this, 10.0F));
+		this.goalSelector.add(6, new GolemMoveToHealGoal(this, 10.0F, 5.0F));
 	}
 	
 	@Override
