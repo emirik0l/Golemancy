1b43ead63616de1dac86cb26bad4168c1631d063
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-11 18:06:38 +0000

    Update GolemancyItemGroup.java
    All soulstones in the creative menu now have perfect stats, and the new "Rustic" soulstone is included

--- src/main/java/net/emirikol/golemancy/GolemancyItemGroup.java
+++ src/main/java/net/emirikol/golemancy/GolemancyItemGroup.java
@@ -22,16 +22,19 @@ public class GolemancyItemGroup {
 			stacks.add(new ItemStack(Golemancy.TERRACOTTA_EFFIGY));
 			stacks.add(new ItemStack(Golemancy.SOULSTONE_EMPTY));
 			List<Genome> genomes = Arrays.asList(
-				Genomes.ENDERMAN,
-				Genomes.SLIME,
-				Genomes.CREEPER,
-				Genomes.ZOMBIE,
-				Genomes.SHULKER,
-				Genomes.BLAZE,
-				Genomes.SKELETON,
-				Genomes.PHANTOM,
-				Genomes.RAVAGER,
-				Genomes.GHAST
+				//Natural genomes
+				creativeGenome(SoulTypes.COVETOUS),
+				creativeGenome(SoulTypes.CURIOUS),
+				creativeGenome(SoulTypes.ENTROPIC),
+				creativeGenome(SoulTypes.HUNGRY),
+				creativeGenome(SoulTypes.INTREPID),
+				creativeGenome(SoulTypes.PARCHED),
+				creativeGenome(SoulTypes.RESTLESS),
+				creativeGenome(SoulTypes.TACTILE),
+				creativeGenome(SoulTypes.VALIANT),
+				creativeGenome(SoulTypes.WEEPING),
+				//Mutated genomes
+				creativeGenome(SoulTypes.RUSTIC)
 			);
 			for(Genome genome: genomes) {
 				ItemStack stack = new ItemStack(Golemancy.SOULSTONE_FILLED);
@@ -40,5 +43,17 @@ public class GolemancyItemGroup {
 			}
 		})
 		.build();
+	}
+
+	public static Genome creativeGenome(SoulType soulType) {
+		//Create a "perfect" genome of the given type, for use in the creative menu.
+		return new Genome() {{
+			put("type", new Gene<SoulType>(soulType));
+			put("potency", new Gene<Integer>(5));
+			put("strength", new Gene<Integer>(3));
+			put("agility", new Gene<Integer>(3));
+			put("vigor", new Gene<Integer>(3));
+			put("smarts", new Gene<Integer>(3));
+		}};
 	}
 }
