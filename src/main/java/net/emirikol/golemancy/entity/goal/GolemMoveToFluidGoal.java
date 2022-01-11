40eb0ebd9e11a1bc22eb4737deec385f003d4646
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-11 13:42:35 +0000

    Make GolemMoveToFluidGoal look at target

--- src/main/java/net/emirikol/golemancy/entity/goal/GolemMoveToFluidGoal.java
+++ src/main/java/net/emirikol/golemancy/entity/goal/GolemMoveToFluidGoal.java
@@ -2,17 +2,22 @@ package net.emirikol.golemancy.entity.goal;
 
 import net.emirikol.golemancy.entity.*;
 
+import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.fluid.*;
 import net.minecraft.util.math.*;
 import net.minecraft.server.world.*;
 
+import java.util.EnumSet;
+
 public class GolemMoveToFluidGoal extends GolemMoveGoal {
 	public GolemMoveToFluidGoal(AbstractGolemEntity entity, float searchRadius) {
 		super(entity, searchRadius, 1);
+		this.setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.LOOK));
 	}
 	
 	public GolemMoveToFluidGoal(AbstractGolemEntity entity, float searchRadius, float maxYDifference) {
 		super(entity, searchRadius, maxYDifference);
+		this.setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.LOOK));
 	}
 	
 	@Override
@@ -22,7 +27,15 @@ public class GolemMoveToFluidGoal extends GolemMoveGoal {
 
 	@Override
 	public boolean shouldContinue() { return GolemHelper.hasEmptyBucket(this.entity) && super.shouldContinue(); }
-	
+
+	@Override
+	public void tick() {
+		//Attempt to look at fluid.
+		this.entity.getLookControl().lookAt(this.targetPos.getX(), this.targetPos.getY(), this.targetPos.getZ());
+		//Continue towards targetPos.
+		super.tick();
+	}
+
 	@Override
 	public boolean isTargetPos(BlockPos pos) {
 		ServerWorld world = (ServerWorld) this.entity.world;
