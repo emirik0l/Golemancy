f01848988d4ad745c6e8d956be47b0bec9146167
ofasgard <77687278+emirik0l@users.noreply.github.com>
2022-01-16 22:06:49 +0000

    add a mutation to make verdant golem obtainable

--- src/main/java/net/emirikol/golemancy/genetics/Mutations.java
+++ src/main/java/net/emirikol/golemancy/genetics/Mutations.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Mutations {
     public static final Mutation RUSTIC = new Mutation(SoulTypes.ENTROPIC, SoulTypes.COVETOUS, SoulTypes.RUSTIC, 0.10F);
+    public static final Mutation VERDANT = new Mutation(SoulTypes.TACTILE, SoulTypes.COVETOUS, SoulTypes.VERDANT, 0.10F);
 
-    public static final List<Mutation> MUTATIONS = Arrays.asList(RUSTIC);
+    public static final List<Mutation> MUTATIONS = Arrays.asList(RUSTIC, VERDANT);
 }
