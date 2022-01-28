58336e6b1b9fc28fbfee69e691cbc3a92eaca21f
emirik0l <77687278+emirik0l@users.noreply.github.com>
2022-01-28 19:55:02 +0000

    Refactor GolemEntityModel animations
    put animations in their own methods to improve readability

--- src/main/java/net/emirikol/golemancy/client/model/GolemEntityModel.java
+++ src/main/java/net/emirikol/golemancy/client/model/GolemEntityModel.java
@@ -15,21 +15,31 @@ public class GolemEntityModel extends AbstractGolemEntityModel<AbstractGolemEnti
 	@Override
 	public void setAngles(AbstractGolemEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		super.setAngles(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
-		
+
 		//Attacking animation.
-		int i = entity.getAttackTicksLeft();
-		if (i > 0) {
-			ModelPart attackArm = this.getArm(entity.getMainArm());
-			float attackPitch = -1.0F + 1.5F * MathHelper.wrap((float)i, 10.0F);
-			setRotationAngle(attackArm, attackPitch, 0, 0);
+		if (entity.getAttackTicksLeft() > 0) {
+			this.attackAnimation(entity);
+			return;
 		}
 		
 		//Arm swinging animation.
-		int j = entity.getSwingTicksLeft();
-		if (j > 0) {
-			float swingPitch = -0.5F + MathHelper.wrap((float)j, 1.25F);
-			setRotationAngle(this.getArm(Arm.LEFT), swingPitch, 0, 0);
-			setRotationAngle(this.getArm(Arm.RIGHT), swingPitch, 0, 0);
+		if (entity.getSwingTicksLeft() > 0) {
+			this.armSwingAnimation(entity);
+			return;
 		}
+	}
+
+	public void attackAnimation(AbstractGolemEntity entity) {
+		int i = entity.getAttackTicksLeft();
+		ModelPart attackArm = this.getArm(entity.getMainArm());
+		float attackPitch = -1.0F + 1.5F * MathHelper.wrap((float)i, 10.0F);
+		setRotationAngle(attackArm, attackPitch, 0, 0);
+	}
+
+	public void armSwingAnimation(AbstractGolemEntity entity) {
+		int i = entity.getSwingTicksLeft();
+		float swingPitch = -0.5F + MathHelper.wrap((float)i, 1.25F);
+		setRotationAngle(this.getArm(Arm.LEFT), swingPitch, 0, 0);
+		setRotationAngle(this.getArm(Arm.RIGHT), swingPitch, 0, 0);
 	}
 }
