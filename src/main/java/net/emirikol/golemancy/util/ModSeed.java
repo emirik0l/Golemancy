3165839f77d4bf2e95b5bcac348b61bb4b984ce3
Kilian Payer <kilian.payer@protonmail.com>
2022-02-04 01:18:58 +0000

    add stemaria support

--- src/main/java/net/emirikol/golemancy/util/ModSeed.java
+++ src/main/java/net/emirikol/golemancy/util/ModSeed.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 public class ModSeed{
 
   public static final String CROPARIA_IDENTIFIER = "com.croparia.mod.item.CropsSeed";
+  public static final String STEMARIA_IDENTIFIER = "com.croparia.mod.item.CropsSeed";
 
   public static boolean isModSeed(ItemStack stack){
     boolean croparia = stack.getItem().getClass().getName().equals(CROPARIA_IDENTIFIER);
