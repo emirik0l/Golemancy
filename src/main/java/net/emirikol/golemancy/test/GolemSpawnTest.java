e407be04aea0a68329b2aa14960c8ea8b1c1b139
emirik0l <77687278+emirik0l@users.noreply.github.com>
2022-02-12 22:20:16 +0000

    Refactor GolemSpawnTest

--- src/main/java/net/emirikol/golemancy/test/GolemSpawnTest.java
+++ src/main/java/net/emirikol/golemancy/test/GolemSpawnTest.java
@@ -5,47 +5,51 @@ import net.emirikol.golemancy.GolemancyItemGroup;
 import net.emirikol.golemancy.genetics.Genome;
 import net.emirikol.golemancy.genetics.SoulTypes;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.fabricmc.fabric.impl.gametest.FabricGameTestHelper;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 
+import java.lang.reflect.Method;
+
 import static net.emirikol.golemancy.test.Assertions.assertTrue;
 
 public class GolemSpawnTest implements FabricGameTest {
+    ServerWorld world;
+    BlockPos startPos;
+    PlayerEntity player;
+
+    @Override
+    public void invokeTestMethod(TestContext context, Method method) {
+        this.world = context.getWorld();
+        this.startPos = world.getSpawnPos().up();
+        this.player = context.createMockPlayer();
+        FabricGameTestHelper.invokeTestMethod(context, method, this);
+        this.player.kill();
+    }
 
     @GameTest(structureName = FabricGameTest.EMPTY_STRUCTURE)
     public void clayEffigyBlockSpawnsGolem(TestContext context) {
-        // Get necessary parameters from context.
-        ServerWorld world = context.getWorld();
-        BlockPos pos = world.getSpawnPos().up();
-        PlayerEntity player = context.createMockPlayer();
-        Hand hand = player.getActiveHand();
-        BlockHitResult hit = new BlockHitResult(new Vec3d(pos.getX(), pos.getY(), pos.getZ()), Direction.NORTH, pos, false);
-
         // Create a clay effigy block at the specified location.
-        context.setBlockState(pos, Golemancy.CLAY_EFFIGY_BLOCK.getDefaultState());
-        BlockState state = context.getBlockState(pos);
-
+        context.setBlockState(startPos, Golemancy.CLAY_EFFIGY_BLOCK.getDefaultState());
+        BlockState state = context.getBlockState(startPos);
         // Create a Curious soulstone and give it to the dummy player.
         ItemStack stack = new ItemStack(Golemancy.SOULSTONE_FILLED);
         Genome genome = GolemancyItemGroup.creativeGenome(SoulTypes.CURIOUS);
         genome.toItemStack(stack);
-        player.setStackInHand(hand, stack);
-
+        player.setStackInHand(player.getActiveHand(), stack);
         // Call onUse() and check whether it returns a success.
-        ActionResult result = Golemancy.CLAY_EFFIGY_BLOCK.onUse(state, world, pos, player, hand, hit);
+        BlockHitResult hit = new BlockHitResult(new Vec3d(startPos.getX(), startPos.getY(), startPos.getZ()), Direction.NORTH, startPos, false);
+        ActionResult result = Golemancy.CLAY_EFFIGY_BLOCK.onUse(state, world, startPos, player, player.getActiveHand(), hit);
         assertTrue(result == ActionResult.SUCCESS, "Clay Effigy Block did not return ActionResult.SUCCESS when spawning a golem!");
-
         context.complete();
     }
 }
